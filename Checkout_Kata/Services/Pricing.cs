0a6dcb440a1306fcc2293bc278e3c3df6588d2fd
AvitaJoyline <avitajdsouza@gmail.com>
2024-07-26 17:57:06 +0100

    Added interface and models

---
+++ Checkout_Kata/Services/Pricing.cs
@@ -0,0 +1,29 @@
+﻿using Checkout_Kata.Models;
+
+namespace Checkout_Kata.Services
+{
+    public class Pricing:IPricing
+    {
+        private List<Price> _pricing = new List<Price>();
+
+            /// <summary>
+            /// Gets the unit price of a product by its SKU.
+            /// </summary>
+            /// <param name="sku">The SKU of the product.</param>
+            /// <returns>The unit price.</returns>
+            public int GetUnitPrice(string sku)
+            {
+                return _pricing.FirstOrDefault(p => p.SKU == sku)?.UnitPrice ?? 0;
+            }
+            /// <summary>
+            /// Gets the offer details for a product by its SKU.
+            /// </summary>
+            /// <param name="sku">The SKU of the product.</param>
+            /// <returns>The offer details (quantity and price).</returns>
+            public (int? Quantity, int? Price) GetOffer(string sku)
+            {
+                var rule = _pricing.FirstOrDefault(p => p.SKU == sku);
+                return (rule?.Quantity, rule?.OfferPrice);
+            }
+    }
+}
